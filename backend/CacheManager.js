a1a12d8d647e95b70b757fb5551913cd2a3c90d7
Jean-Baptiste ROUSSEAU <jeanbaptiste1981_75@hotmail.fr>
2017-11-22 22:03:09 +0000

    standard js

--- backend/CacheManager.js
+++ backend/CacheManager.js
@@ -1,24 +1,22 @@
 const LRUCache = require('lru-cache')
 
-
-function CacheManager(dev) {
-
-	// This is where we cache our rendered HTML pages
-	this.ssrCache = new LRUCache({
-	  max: 100,
-	  maxAge: 1000 * 60 * 60 // 1hour
-	})
-	this.dev = dev
+function CacheManager (dev) {
+  // This is where we cache our rendered HTML pages
+  this.ssrCache = new LRUCache({
+    max: 100,
+    maxAge: 1000 * 60 * 60 // 1hour
+  })
+  this.dev = dev
 }
 /*
  * NB: make sure to modify this to take into account anything that should trigger
  * an immediate page change (e.g a locale stored in req.session)
  */
-CacheManager.prototype.getCacheKey = function(req) {
+CacheManager.prototype.getCacheKey = function (req) {
   return `${req.url}`
 }
 
-CacheManager.prototype.renderAndCache = function(app, req, res, pagePath, queryParams) {
+CacheManager.prototype.renderAndCache = function (app, req, res, pagePath, queryParams) {
   const key = this.getCacheKey(req)
 
   // If we have a page in the cache, let's serve it
