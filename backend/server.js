32ebb35e83a95d9e74ec3e3988f4f7b6ce98c0cd
Jean-Baptiste ROUSSEAU <jeanbaptiste1981_75@hotmail.fr>
2017-11-25 17:29:09 +0000

    add helmet, improving ssr...

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const postgraphql = require('postgraphql').postgraphql
 const { parse } = require('url')
 const { join } = require('path')
 const CacheManager = require('./CacheManager')
+const compression = require('compression')
+const helmet = require('helmet')
+
 const { DB_STRING, DB_SCHEMA, SECRET, DEFAULT_ROLE, JWT_TOKEN_IDENTIFIER, GRAPHQL_ENDPOINT } = require('./config')
 
 fetch = require('node-fetch') // eslint-disable-line
@@ -29,6 +32,12 @@ app.prepare()
     extendedErrors: dev ? ['detail'] : [],
     showErrorStack: dev
   }))
+  if (!dev) {
+    // gzip the result
+    server.use(compression())
+    // security library
+    server.use(helmet())
+  }
 
   // Use the `renderAndCache` utility defined below to serve pages
   server.get('/', (req, res) => {
--- components/container/RecipeDetailContainer.js
+++ components/container/RecipeDetailContainer.js
@@ -6,6 +6,10 @@ import {Component} from 'react'
 
 class RecipeDetailContainer extends Component {
   componentDidUpdate () {
+    this.updateTitleTopBar()
+  }
+
+  updateTitleTopBar () {
     if (this.props.data.recipe && typeof this.props.onLoad === 'function') {
       this.props.onLoad(this.props.data.recipe.title)
     }
@@ -14,6 +18,7 @@ class RecipeDetailContainer extends Component {
   render () {
     const recipe = this.props.data.recipe
     if (recipe) {
+      this.updateTitleTopBar()
       //  const areMoreRecipes = allRecipes.totalCount < _allRecipesMeta.count
       return (<RecipeCardDetail recipe={recipe} />)
     }
